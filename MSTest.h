128823db7e643efe42b6b68593f5f282ab1ad2f5
TheSuzukis <admin@Malcolms-MacBook-Pro.local>
2014-06-24 13:37:21 -0400

    added a test class

---
+++ MSTest.h
@@ -0,0 +1,13 @@
+//
+//  MSTest.h
+//  Git Test
+//
+//  Created by TheSuzukis on 2014-06-24.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSTest : NSObject
+
+@end
---
+++ MSTest.m
@@ -0,0 +1,13 @@
+//
+//  MSTest.m
+//  Git Test
+//
+//  Created by TheSuzukis on 2014-06-24.
+//
+//
+
+#import "MSTest.h"
+
+@implementation MSTest
+
+@end
